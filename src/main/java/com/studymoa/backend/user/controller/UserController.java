5b0fef4d94f3fadc43a117f84aa7d9fd099cb973
sungjin <sungjinny5@naver.com>
2026-04-08 14:48:40 +0900

    기능 추가 - API Response 응답방식을 통일하여 프로젝트에 적용

--- src/main/java/com/studymoa/backend/user/controller/UserController.java
+++ src/main/java/com/studymoa/backend/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.studymoa.backend.user.controller;
 
+import com.studymoa.backend.common.response.ApiResponse;
 import com.studymoa.backend.user.dto.LoginRequest;
 import com.studymoa.backend.user.dto.RegisterUser;
 import com.studymoa.backend.user.service.UserService;
@@ -15,14 +16,14 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody RegisterUser request) {
+    public ResponseEntity<ApiResponse<Void>> register(@RequestBody RegisterUser request) {
         userService.register(request.getLoginId(), request.getName(), request.getEmail(), request.getPassword());
-        return ResponseEntity.ok("회원가입이 완료되었습니다.");
+        return ResponseEntity.ok(ApiResponse.ok("회원가입이 완료되었습니다.", null));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<ApiResponse<Void>> login(@RequestBody LoginRequest request) {
         userService.login(request.getLoginId(), request.getPassword());
-        return ResponseEntity.ok("로그인이 완료되었습니다.");
+        return ResponseEntity.ok(ApiResponse.ok("로그인이 완료되었습니다.", null));
     }
 }
